75395ea40f7a210a0555092e921df34eb095e0e7
unknown <Tzybulko.i@inbox.ru>
2020-09-21 15:16:12 +0300

    first commit

--- include/header.hpp
+++ include/header.hpp
@@ -26,72 +26,72 @@ using std::vector;
 
 
 class hashMaster {
- public:
-  uint64_t threadNumber;
-  vector <std::thread> allThreads;
-
-  hashMaster() {
-    threadNumber = std::thread::hardware_concurrency();
-    setThreads();
-  }
-
-  explicit hashMaster(int number) {
-    threadNumber = number;
-    setThreads();
-  }
-
-
-
-  void setThreads() {
-    for (uint64_t i = 0; i < threadNumber; i++) {
-      std::thread th(hashHandler, i, this);
-      allThreads.push_back(std::move(th));
+public:
+    uint64_t threadNumber;
+    vector <std::thread> allThreads;
+    
+    hashMaster() {
+        threadNumber = std::thread::hardware_concurrency();
+        setThreads();
     }
-    for (uint64_t i = 0; i < threadNumber; i++) {
-      allThreads.at(i).join();
+
+    explicit hashMaster(int number) {
+        threadNumber = number;
+        setThreads();
     }
-  }
 
 
 
-  static void hashHandler(int a, hashMaster *obj) {
-    obj->init_logging();
-    while (true){
-      int stringLenth = 30;
-      std::thread::id threadID=obj->allThreads.at(a).get_id();
-      string stringForHash;
-      for (int i = 0; i < stringLenth; ++i) {
-        stringForHash.push_back(std::rand()%255);
-      }
-      const std::string hash = picosha2::hash256_hex_string(stringForHash);
-      if (hash.find("0000", 60) == 60) {
-
-        BOOST_LOG_TRIVIAL(info) << stringForHash << " - " << hash << " - " << threadID << endl;
-        break;
-      }
-      else
-        BOOST_LOG_TRIVIAL(trace) << stringForHash << " - " << " - " << hash << " - " << threadID << endl;
+    void setThreads() {
+        for (uint64_t i = 0; i < threadNumber; i++) {
+            std::thread th(hashHandler, i, this);
+            allThreads.push_back(std::move(th));
+        }
+        for (uint64_t i = 0; i < threadNumber; i++) {
+            allThreads.at(i).join();
+        }
     }
-  }
 
-  static void init_logging() {
 
-    logging::add_file_log (
-        keywords::file_name = "info.log",
-        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-    );
 
-    logging::add_console_log (
-        std::cout,
-        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-    );
+    static void hashHandler(int a, hashMaster *obj) {
+        obj->init_logging();
+        while (true){
+            int stringLenth = 30;
+            std::thread::id threadID=obj->allThreads.at(a).get_id();
+            string stringForHash;
+            for (int i = 0; i < stringLenth; ++i) {
+                stringForHash.push_back(std::rand()%255);
+            }
+            const std::string hash = picosha2::hash256_hex_string(stringForHash);
+            if (hash.find("0000", 60) == 60) {
+                
+                BOOST_LOG_TRIVIAL(info) << stringForHash << " - " << hash << " - " << threadID << endl;
+                break;
+            }
+            else
+                BOOST_LOG_TRIVIAL(trace) << stringForHash << " - " << " - " << hash << " - " << threadID << endl;
+        }
+    }
 
-    logging::add_common_attributes();
-  }
+    static void init_logging() {
+        
+        logging::add_file_log (
+                        keywords::file_name = "info.log",
+                        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+                );
+
+        logging::add_console_log (
+                std::cout,
+                keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+        );
+
+        logging::add_common_attributes();
+    }
 };
 
 int main() {
-  hashMaster test(4);
+    hashMaster test(4);
 }
 
 #endif // INCLUDE_HEADER_HPP_
